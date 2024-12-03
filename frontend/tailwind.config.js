c9757761381aeed90433cb49be3ec79e1a7fe2a1
Sohail A <107506915+thesameninja@users.noreply.github.com>
2024-12-03 11:29:54 -0500

    Fixed issues on frontend
    -Updated outdated packages, fixed tailwind src files path.

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,7 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "/Users/amimularnab/notesage/frontend/app/**/*.{js,jsx,ts,tsx}", 
+    "./app/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}",
+    "./pages/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {},
